e513ffebe751c8816b4e177229cecb78cac385e8
Kyran <40345234+kyraaan@users.noreply.github.com>
2024-07-13 12:41:40 +0100

    Initial project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Digiseq Web Portal
---
+++ src/test/java/com/digiseq/digiseqwebportal/WebPortalApplicationTests.java
@@ -0,0 +1,11 @@
+package com.digiseq.digiseqwebportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortalApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
