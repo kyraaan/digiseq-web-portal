fe6cc22b075819437fa0fc3065f528ee60571117
Kyran <40345234+kyraaan@users.noreply.github.com>
2024-07-15 15:51:18 +0100

    Fix migrations file + Add initial data insertion

---
+++ src/main/resources/db/migration/V1_2__insert_data.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO ClientOrg (name, registered_date, expiry_date, enabled)
+VALUES
+    ('Client 1', '2023-01-01', '2024-12-31', true),
+    ('Client 2', '2023-02-01', '2025-01-31', true);
+
+-- Insert initial data into Personnel
+INSERT INTO Personnel (username, first_name, last_name, password, email, phone_number, client_id)
+VALUES
+    ('user1', 'John', 'Doe', 'password1', 'john.doe@example.com', '1234567890', 1),
+    ('user2', 'Jane', 'Smith', 'password2', 'jane.smith@example.com', '9876543210', 2);
